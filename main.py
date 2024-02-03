30e197be5e648f44b967876fe697ccdc17956d4a
nikodim500 <nikolay@chetverikov.pro>
2024-02-03 12:46:48 +0000

    initial load

---
+++ main.py
@@ -0,0 +1,38 @@
+# main.py -- put your code here!
+
+import machine
+from time import sleep
+import dht 
+
+#sensor = dht.DHT22(machine.Pin(14))
+sensor = dht.DHT11(machine.Pin(5))
+
+def deep_sleep(msecs):
+  #configure RTC.ALARM0 to be able to wake the device
+  rtc = machine.RTC()
+  rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+  # set RTC.ALARM0 to fire after Xmilliseconds, waking the device
+  rtc.alarm(rtc.ALARM0, msecs)
+  #put the device to sleep
+  machine.deepsleep()
+
+temperature = -273
+humidity = -100
+
+def send_measures():
+  print('Temperature: {} C'.format(temperature))
+  print('Humidity: {} %'.format(humidity))
+
+
+while True:
+  try:
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    temperature = temp
+    humidity = hum
+    send_measures()
+    #deep_sleep(300000)
+  except OSError as e:
+    print('Failed to read sensor.')
+  sleep(2)
