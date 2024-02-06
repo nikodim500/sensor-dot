bde250d4053112c9d2f1d820a0a621d757fc2e89
nikodim500 <nikolay@chetverikov.pro>
2024-02-06 18:10:43 +0000

    WiFi connection

--- boot.py
+++ boot.py
@@ -1,2 +1,33 @@
 # boot.py -- run on boot-up
-# Add WiFi initiation and time sync
+# Add time sync
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+import network
+import esp
+import gc
+import scrt
+
+esp.osdebug(None)
+gc.collect()
+
+ssid = scrt.SSID
+wifi_password = scrt.WIFIPWD
+mqtt_server = scrt.MQTTSERVER
+mqtt_user = scrt.MQTTUSER
+mqtt_password = scrt.MQTTPWD
+client_id = ubinascii.hexlify(machine.unique_id()).decode()
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, wifi_password)
+
+while station.isconnected() == False:
+  print('/r' + time.localtime()[5], end='')
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
