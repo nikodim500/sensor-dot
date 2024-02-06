4ad0c1592b8ee02e5a50ddd6e915ae89eef99468
nikodim500 <nikolay@chetverikov.pro>
2024-02-06 18:41:01 +0000

    added ntp time sync

--- boot.py
+++ boot.py
@@ -10,6 +10,8 @@ import network
 import esp
 import gc
 import scrt
+import ntptime
+
 
 esp.osdebug(None)
 gc.collect()
@@ -26,12 +28,15 @@ def connect_wifi():
     print('')
     print('Connection successful')
     print(station.ifconfig())
+    ntptime.settime()
+    t = time.localtime
+    print('NTP time syncronized. UTC time: ' + t[2] + '/' + t[1] + '/' + t[0] + ' ' + t[3] + ':' + t[4] + ':' + t[5])
 
 def connect_mqtt():
     global device_id
     client = MQTTClient(device_id, scrt.MQTTSERVER, 0, scrt.MQTTUSER, scrt.MQTTPWD)
     client.connect()
-    print('Connected to {} MQTT broker'.format(mqtt_server))
+    print('Connected to {}:{} MQTT broker'.format(client.server, client.port))
     return client
 
 connect_wifi()
